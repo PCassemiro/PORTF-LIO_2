c748da95575fd18db9b690d54d838b4e2cbbdbfb
Pedro de Oliveira Cassemiro <pedrobr2909@gmail.com>
2025-01-23 13:40:14 -0300

    add font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['"Montserrat"', "sans-serif"], // Define como padrão para 'font-sans'
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
